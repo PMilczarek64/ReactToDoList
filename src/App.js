316a919954e0e4d10eb15412c1e7b7966abd7488
Mammoth64 <mammoth1907@gmail.com>
2022-04-22 20:18:01 +0200

    Add first components and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero/Hero";
 import SearchForm from "./components/SearchFrom/SearchFrom";
+import List from "./components/List/List";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <SearchForm />
+      <List />
     </div>
   );
 };
