46778717edef823d8c3441f27d3cf87f0f1afe25
Mammoth64 <mammoth1907@gmail.com>
2022-04-27 23:35:34 +0200

    Add ColumnForm component and Update List.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,8 +1,8 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column';
 import { useState } from 'react';
-import { useEffect } from 'react';
 import shortid from 'shortid';
+import ColumnForm from '../ColumnForm/ColumnForm';
 
 const List = () => {
 
@@ -12,13 +12,9 @@ const List = () => {
       { id: 3, title: 'Games', icon: 'gamepad' }
   ]);
 
-  const [value, setValue] = useState('');
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    setColumns([...columns, { id: shortid(), title: value }]);
-    setValue('');
-  }; 
+  const addColumn = newColumn => {
+		setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+};
 
   return (
     <div className={styles.list}>
@@ -29,10 +25,7 @@ const List = () => {
       <section className={styles.columns}>
         {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
       </section>
-      <form onSubmit={handleSubmit}>
-      <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-        <button>Add column</button>
-      </form>
+      <ColumnForm action={addColumn}/>
     </div>
   );
  }; 
